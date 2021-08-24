8f98f8d6a9c78fbc65d4f4bb745753707a4808f2
Filipe Mariz <filipeferreiramariz@outlook.com>
2021-08-24 19:12:37 -0300

    feat: created a component for header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { Header } from './styles';
+
+const Headers = () => {
+  return (
+    <>
+      <Header>
+        <img className="kuppiImage" src="https://sitebooster-appsharp.netdna-ssl.com/i/6005b9e643e1e640535a08a7/2a856398fb5049129716b1929930ed16/f=JPEG" alt="kuppi" />
+        <h1 className="Title">KuppiStore</h1>
+        <h3 className="SubTitle">Um serviço do grupo Kuppi</h3>
+      </Header>
+    </>
+  )
+}
+
+export default Headers
