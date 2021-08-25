c69b14e9af5291776e31c6f60583fd6da15cb716
Filipe Mariz <filipeferreiramariz@outlook.com>
2021-08-25 16:59:15 -0300

    chore: added map

--- src/pages/ListProducts/index.tsx
+++ src/pages/ListProducts/index.tsx
@@ -5,6 +5,7 @@ import Headers from "../../components/header";
 
 import api from '../../services/api'
 import Button from "../../components/Button";
+import SelectorPoducts from '../../components/itens'
 interface Products {
   name: string;
   price: number;
@@ -14,41 +15,12 @@ interface Products {
 }
 
 const ListPoducts = () => {
-  const [displayModal, setDisplayModal] = useState(false);
-  const [data, setData] = useState<Products[]>([])
-
-  const handleGettingPatientsData = async () => {
-    try {
-      const apiResponse = await api.get('/example/products/')
-      if (apiResponse.data) {
-        setData(apiResponse.data)
-      }
-    } catch (err) {
-      console.log(err.message);
-    }
-  }
-
-  useEffect(() => {
-    handleGettingPatientsData();
-  }, []);
-
   return (
     <>
       <Container>
         <Headers />
         <Label><h1>Encontre um produto para você</h1></Label>
-        {data.map((product) => {
-          return (
-            <SelectorDiv>
-              <label className="Name">{product.name}</label>
-              <img className="kuppiImage" src={product.photo_url} alt="kuppi" />
-              <label className="Price">R${product.price}</label>
-              <Button isSecondary={false} onClick={() => setDisplayModal(true)}>
-                Detalhes
-              </Button>
-            </SelectorDiv>
-          )
-        })}
+        <SelectorPoducts />
       </Container>
     </>
   )
