a3618fd2911c787aef0a0ec7603ee55fd7e704eb
Filipe Mariz <filipeferreiramariz@outlook.com>
2021-08-24 21:50:00 -0300

    feat: created a component

---
+++ src/components/itens/index.tsx
@@ -0,0 +1,16 @@
+import { SelectorDiv } from './styles';
+
+const SelectorPoducts = () => {
+  return (
+    <>
+      <SelectorDiv>
+        <label className="Name">Coca-cola 250ml</label>
+        <img className="kuppiImage" src="https://static.carrefour.com.br/medias/sys_master/images/images/h3a/h3e/h00/h00/12175734472734.jpg" alt="kuppi" />
+        <button>Detalhes</button>
+      </SelectorDiv>
+    </>
+  )
+
+}
+
+export default SelectorPoducts;
