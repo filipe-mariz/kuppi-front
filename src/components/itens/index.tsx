d13222595b3c32c301c77c8d49e70c32c5cacb77
Filipe Mariz <filipeferreiramariz@outlook.com>
2021-08-25 08:57:11 -0300

    feat: added component styles of itens

--- src/components/itens/index.tsx
+++ src/components/itens/index.tsx
@@ -1,13 +1,22 @@
 import { SelectorDiv } from './styles';
+import Button from '../Button';
+import { useState } from 'react';
+
+import Modal from '../modals/ProductsModal';
 
 const SelectorPoducts = () => {
+  const [displayModal, setDisplayModal] = useState(false);
+
   return (
     <>
       <SelectorDiv>
         <label className="Name">Coca-cola 250ml</label>
         <img className="kuppiImage" src="https://static.carrefour.com.br/medias/sys_master/images/images/h3a/h3e/h00/h00/12175734472734.jpg" alt="kuppi" />
         <label className="Price">R$ 2,50</label>
-        <button>Detalhes</button>
+        <Button isSecondary={false} onClick={() => setDisplayModal(true)}>
+          Detalhes
+        </Button>
+        {displayModal ? <Modal modalDismiss={() => setDisplayModal(false)} /> : null}
       </SelectorDiv>
     </>
   )
